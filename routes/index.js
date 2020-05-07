513852a8debc83a03d5fdfa31a03ff6ae76c6928
Ahmed Maruf <maruf@cefalo.com>
2020-05-07 18:09:31 +0600

    Handling flash messages

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,6 @@ const router = express.Router();
 const StoreController = require('../controllers/StoreController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-exports.catchErrors = (fn) => {
-	return function(req, res, next) {
-		return fn(req, res, next).catch(next);
-	};
-};
-
 // Do work here
 router.get('/', StoreController.homePage);
 router.get('/add', StoreController.addStore);
