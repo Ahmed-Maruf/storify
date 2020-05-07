7d697e970c1a2717eb2f95c084a52d61ebcaac8c
Ahmed Maruf <maruf@cefalo.com>
2020-05-07 17:53:38 +0600

    Add create method for creating data related to stores

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const StoreController = require('../controllers/StoreController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
+exports.catchErrors = (fn) => {
+	return function(req, res, next) {
+		return fn(req, res, next).catch(next);
+	};
+};
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
-
+router.get('/', StoreController.homePage);
+router.get('/add', StoreController.addStore);
+router.post('/add', catchErrors(StoreController.createStore));
 module.exports = router;
