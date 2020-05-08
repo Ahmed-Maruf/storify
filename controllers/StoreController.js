ee872b9d4cf59ebc931f117e1937658780c1cfbd
Ahmed Maruf <maruf@cefalo.com>
2020-05-09 01:45:53 +0600

    View a single store

--- controllers/StoreController.js
+++ controllers/StoreController.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+	storage: multer.memoryStorage(),
+	fileFilter(req, file, next) {
+		const isPhoto = file.mimetype.startsWith('image');
+		console.log(file.mimetype);
+		if (isPhoto) {
+			next(null, true);
+		} else {
+			next({message: 'That filetype isn\'t supported'}, false);
+		}
+	}
+}
+
 exports.homePage = (req, res) => {
 	res.render('index');
 }
@@ -8,6 +25,23 @@ exports.addStore = (req, res) => {
 	res.render('edit-store', {
 		title: 'Add Store'
 	});
+}
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+	if (!req.file) {
+		next();
+		return;
+	}
+	const extension = req.file.mimetype.split('/')[1];
+	req.body.photo = `${uuid.v4()}.${extension}`;
+	const photo = await jimp.read(req.file.buffer);
+	await photo.resize(800, jimp.AUTO);
+	await photo.write(`./public/uploads/${req.body.photo}`);
+	console.log(photo);
+	next();
+	
 }
 
 exports.createStore = async (req, res) => {
@@ -25,7 +59,7 @@ exports.getStores = async (req, res) => {
 }
 
 exports.editStore = async (req, res) => {
-	const store = await Store.findById({ _id: req.params.id });
+	const store = await Store.findById({_id: req.params.id});
 	res.render('edit-store', {
 		title: `Edit ${store.name}`,
 		store
@@ -33,11 +67,20 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
-	const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+	const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
 		new: true,
 		runValidators: true
 	}).exec();
 	
 	req.flash('success', `Successfully updated ${store.name} view store <a href="${store._id}">click</a>`);
 	res.redirect(`/stores/${store.slug}/edit`);
+}
+
+exports.getStoreBySlug = async (req, res) => {
+	const store = await Store.findOne({ slug: req.params.slug });
+	if (!store) return next();
+	res.render('store', {
+		title: store.name,
+		store
+	})
 }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const StoreController = require('../controllers/StoreController');
-const { catchErrors } = require('../handlers/errorHandlers');
-
+const {catchErrors} = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', StoreController.getStores);
 router.get('/stores', StoreController.getStores);
 router.get('/store/:id/edit', catchErrors(StoreController.editStore));
 router.get('/add', StoreController.addStore);
-router.post('/add', catchErrors(StoreController.createStore));
-router.post('/add/:id', catchErrors(StoreController.updateStore));
+router.post(
+	'/add',
+	StoreController.upload,
+	catchErrors(StoreController.resize),
+	catchErrors(StoreController.createStore)
+);
+router.post(
+	'/add/:id',
+	StoreController.upload,
+	catchErrors(StoreController.resize),
+	catchErrors(StoreController.updateStore)
+);
+
+router.get('/store/:slug', catchErrors(StoreController.getStoreBySlug));
+
 
 module.exports = router;
